121f5dc7445b4d4c21303f9c3b644f90ba60e8c9
julek <juliusz.sosinowicz@atteli.com>
2018-10-21 05:43:02 +0200

    konstytucja

--- WebServer/api/views.py
+++ WebServer/api/views.py
@@ -18,7 +18,7 @@ from django.shortcuts import render
 
 # Login ApiView
 from rest_framework import status
-from rest_framework.decorators import list_route
+from rest_framework.decorators import list_route, permission_classes
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -218,6 +218,7 @@ class RouteViewSet(ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+    @permission_classes((AllowAny,))
     @list_route(methods=['GET'])
     def all_routes_path(self, request, *args, **kwargs):
         drivers = Driver.objects.all()
